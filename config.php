8f4a3b5b72a91d526951d94a7a6422c5e55ace7a
alfatpololo <alfat.pololo@gmail.com>
2024-01-06 05:20:09 +0700

    pertamax

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+//create connection
+$koneksi = mysqli_connect("localhost", "rootie", "", "db_sekolah");
+
+//connection check
+//if (mysqli_connect_errno()) {
+//    echo "Failed to connect database";
+//} else {
+//    echo "Connected to database";
+//}
+
+//parent url
+$main_url = "http://localhost/sekolah/";
+
+?>
