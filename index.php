8363aa2e2c258f65fc4d1940ca4617c0b546e160
alfatpololo <alfat.pololo@gmail.com>
2024-01-06 05:33:18 +0700

    ulala

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ require_once "template/sidebar.php";
                     </div>
                     <div class="col-xl-3 col-md-6">
                         <div class="card bg-danger text-white mb-4">
-                            <div class="card-body">Jumlah Siswaw Gagal Ujian</div>
+                            <div class="card-body">Jumlah Siswa Gagal Ujian</div>
                             <div class="card-footer d-flex align-items-center justify-content-between">
                                 <a class="small text-white stretched-link" href="#">0 Orang</a>
                                 <div class="small text-white"><i class="fas fa-angle-right"></i></div>
