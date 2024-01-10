af00dbaeeb63418eba156322565b181aa3c32aef
mrxy <alfat.pololo@gmail.com>
2024-01-10 10:06:25 +0700

    add database source

---
+++ db_sekolah.sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 10, 2024 at 04:05 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_guru`
+--
+
+CREATE TABLE `tbl_guru` (
+  `id` int(11) NOT NULL,
+  `nip` varchar(30) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(128) NOT NULL,
+  `telpon` varchar(20) NOT NULL,
+  `agama` varchar(20) NOT NULL,
+  `foto` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_nilai_ujian`
+--
+
+CREATE TABLE `tbl_nilai_ujian` (
+  `id` int(11) NOT NULL,
+  `no_ujian` char(7) NOT NULL,
+  `pelajaran` varchar(100) NOT NULL,
+  `nilai_ujian` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pelajaran`
+--
+
+CREATE TABLE `tbl_pelajaran` (
+  `id` int(11) NOT NULL,
+  `pelajaran` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `guru` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_sekolah`
+--
+
+CREATE TABLE `tbl_sekolah` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `alamat` varchar(128) NOT NULL,
+  `akreditasi` char(1) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `visimisi` varchar(256) NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_siswa`
+--
+
+CREATE TABLE `tbl_siswa` (
+  `nis` char(6) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(128) NOT NULL,
+  `kelas` varchar(3) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ujian`
+--
+
+CREATE TABLE `tbl_ujian` (
+  `no_ujian` char(7) NOT NULL,
+  `tgl_ujian` date NOT NULL,
+  `nis` char(6) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `total_nilai` int(11) NOT NULL,
+  `nilai_terendah` int(11) NOT NULL,
+  `nilai_tertinggi` int(11) NOT NULL,
+  `nilai_rata2` int(11) NOT NULL,
+  `hasil_ujian` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `alamat` varchar(128) NOT NULL,
+  `jabatan` varchar(128) NOT NULL,
+  `foto` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_guru`
+--
+ALTER TABLE `tbl_guru`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_nilai_ujian`
+--
+ALTER TABLE `tbl_nilai_ujian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_pelajaran`
+--
+ALTER TABLE `tbl_pelajaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_sekolah`
+--
+ALTER TABLE `tbl_sekolah`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  ADD PRIMARY KEY (`nis`);
+
+--
+-- Indexes for table `tbl_ujian`
+--
+ALTER TABLE `tbl_ujian`
+  ADD PRIMARY KEY (`no_ujian`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_guru`
+--
+ALTER TABLE `tbl_guru`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_nilai_ujian`
+--
+ALTER TABLE `tbl_nilai_ujian`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_pelajaran`
+--
+ALTER TABLE `tbl_pelajaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_sekolah`
+--
+ALTER TABLE `tbl_sekolah`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
